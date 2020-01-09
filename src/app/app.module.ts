4600823bb70fc06c7ab8428c489b6f405a75efbc
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-01-09 14:12:47 +0800

    Sentry integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule, ErrorHandler, Injectable } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { QRScanner } from '@ionic-native/qr-scanner/ngx';
 import { ModalController, AngularDelegate } from '@ionic/angular';
@@ -11,6 +11,27 @@ import { MyApp } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from './components/components.module';
 import { ScanPageModule } from './pages/scan/scan.module';
+
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://352d312bc39c4f56b6ca26dca76b9be0@sentry.io/1875732"
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+
+  handleError(error) {
+    console.error("Globally catched exception:", error);
+
+    console.log(document.URL);
+    // Only send reports to sentry if we are not debugging.
+    if (document.URL.includes('localhost')) { // Prod builds or --nodebug CLI builds use "http://localhost"
+      Sentry.captureException(error.originalError || error);
+    }
+  }
+}
 
 @NgModule({
   declarations: [
@@ -36,7 +57,7 @@ import { ScanPageModule } from './pages/scan/scan.module';
     AngularDelegate,
     Platform,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    {provide: ErrorHandler, useClass: ErrorHandler}
+    {provide: ErrorHandler, useClass: SentryErrorHandler}
   ]
 })
 export class AppModule {}
