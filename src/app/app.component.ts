cae1c4ffb957f6e729b5a7745cb7f0e80056c2d8
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-03-19 22:38:07 +0800

    Fixed back key navigation to exit app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, IonRouterOutlet } from '@ionic/angular';
 //import { StatusBar } from '@ionic-native/status-bar';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -8,27 +8,46 @@ import { ScanPage } from './pages/scan/scan.page';
 import { AppService } from './app.service';
 import { Router } from '@angular/router';
 
+declare let appManager: AppManagerPlugin.AppManager;
+
 @Component({
   selector: 'my-app',
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(IonRouterOutlet, {static: true}) routerOutlet: IonRouterOutlet;
   rootPage:any = ScanPage;
 
   constructor(
-    platform: Platform, 
+    private platform: Platform, 
     statusBar: StatusBar, 
     splashScreen: SplashScreen, 
     private appService: AppService,
     private router: Router) {
       
-    platform.ready().then(() => {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
 
+      this.setupBackKeyNavigation();
+
       this.router.navigate(["scan"]);
+    });
+  }
+
+  /**
+   * Listen to back key events. If the default router can go back, just go back.
+   * Otherwise, exit the application.
+   */
+  setupBackKeyNavigation() {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      if (this.routerOutlet && this.routerOutlet.canGoBack()) {
+        this.routerOutlet.pop();
+      } else {
+        appManager.close();
+      }
     });
   }
 }
